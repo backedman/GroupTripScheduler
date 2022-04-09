8a94d8e53bf6362a0acc50c028c59805d8d71318
CyberCyber2 <smithcody2@protonmail.com>
2022-04-09 15:11:21 -0400

    amplify integration 1

--- app/src/main/java/com/example/grouptripscheduler/Application.kt
+++ app/src/main/java/com/example/grouptripscheduler/Application.kt
@@ -4,4 +4,10 @@ import android.app.Application
 
 class AndroidGettingStartedApplication : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+        // initialize Amplify when application is starting
+        Backend.initialize(applicationContext)
+    }
 }
