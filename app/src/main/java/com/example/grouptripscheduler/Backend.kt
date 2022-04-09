9bf966000305851b6d8a0d6ca639580247703a86
CyberCyber2 <smithcody2@protonmail.com>
2022-04-09 13:34:42 -0400

    Attempt to resolve classPath errors

--- app/src/main/java/com/example/grouptripscheduler/Backend.kt
+++ app/src/main/java/com/example/grouptripscheduler/Backend.kt
@@ -3,7 +3,9 @@ package com.example.grouptripscheduler
 import android.content.Context
 import android.util.Log
 import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.result.AuthSessionResult
 import com.amplifyframework.core.Amplify
+
 
 object Backend {
 
