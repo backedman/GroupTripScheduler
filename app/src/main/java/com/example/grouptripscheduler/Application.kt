f25fce6b0a30c631bdd9335c595b4b5b1a12a8d1
CyberCyber2 <smithcody2@protonmail.com>
2022-04-09 13:59:31 -0400

    Attempt to resolve classPath errors

--- app/src/main/java/com/example/grouptripscheduler/Application.kt
+++ app/src/main/java/com/example/grouptripscheduler/Application.kt
@@ -4,10 +4,4 @@ import android.app.Application
 
 class AndroidGettingStartedApplication : Application() {
 
-    override fun onCreate() {
-        super.onCreate()
-
-        // initialize Amplify when application is starting
-        Backend.initialize(applicationContext)
-    }
 }
